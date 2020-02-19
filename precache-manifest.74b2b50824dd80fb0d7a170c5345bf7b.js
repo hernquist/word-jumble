37b98be443bd2783e03c6f17c7986562b1106659
dhernquist <david.hernquist@turner.com>
2020-02-18 20:26:37 -0500

    Updates

---
+++ precache-manifest.74b2b50824dd80fb0d7a170c5345bf7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11397f01863bedfd716d20a8cc11764",
+    "url": "/word-jumble/index.html"
+  },
+  {
+    "revision": "bf361a01e6ef5aa46b60",
+    "url": "/word-jumble/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5b69ebf94a69fc488b62",
+    "url": "/word-jumble/static/js/2.002ca734.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/word-jumble/static/js/2.002ca734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf361a01e6ef5aa46b60",
+    "url": "/word-jumble/static/js/main.bfe96a22.chunk.js"
+  },
+  {
+    "revision": "db2d58fa675d7a7a02a7",
+    "url": "/word-jumble/static/js/runtime-main.d6407103.js"
+  }
+]);
